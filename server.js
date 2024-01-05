7631f9af17bbbffd3cb5b094548a96d99c1f8836
prabhath-kj <view.prabhath.kj@gmail.com>
2024-01-05 22:03:18 +0530

    redis configured

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
+import express from "express";
 import client from "./client.js";
-import express from "express"
 
-const app=express()
+const app = express();
 
+app.get("/", async (req, res) => {
+  const cachedData = await client.get("todos");
 
-app.get("/",async(req,res)=>{
-    res.json({message:"hi"})
-})
+  if (cachedData) {
+    console.log("Data retrieved from cache");
+    res.json(JSON.parse(cachedData));
+  } else {
+    console.log("Fetching data from API");
+    const response = await (await fetch('https://jsonplaceholder.typicode.com/todos')).json();
+    
+    // Store data in Redis cache with a timeout of 300 seconds (adjust as needed)
+    client.setex("todos", 300, JSON.stringify(response));
 
-app.listen(3000,()=>{
-    console.log("Server connected")
-})
+    res.json(response);
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server connected");
+});
